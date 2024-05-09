86f96929913b7f240bf004d9689a86b15af526c8
Deividmattos <deividmattos79@hotmail.com>
2024-05-09 20:46:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="css/style.css">
+      <script src="js/script.js" defer></script>
+      <title>Portfolio</title>
+</head>
+
+<body>
+      <header class="header">
+            <div id="menu_btn" class="fas fa-bars menu_btn"></div>
+
+            <a href="#home" class="logo">Portfolio</a>
+            <nav class="navbar">
+                  <a href="#home" class="active">home</a>
+                  <a href="#about">about</a>
+                  <a href="#services">services</a>
+                  <a href="#portfolio">portfolio</a>
+                  <a href="#contact">contact</a>
+            </nav>
+            <div class="social_media">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+                  <a href="#" class="fab fa-github"></a>
+
+            </div>
+      </header>
+</body>
+
+</html>
